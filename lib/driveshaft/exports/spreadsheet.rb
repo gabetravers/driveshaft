ba229bd165ff7af8122e859cf4235ed7f377a94d
Gabe Travers <gtravers@hearst.com>
2017-06-26 16:28:14 -0400

    no message

--- lib/driveshaft/exports/spreadsheet.rb
+++ lib/driveshaft/exports/spreadsheet.rb
@@ -28,7 +28,7 @@ module Driveshaft
       end
 
       return {
-        body: JSON.dump(data),
+        body: JSON.dump(data).slice!(4),
         content_type: 'application/json; charset=utf-8'
       }
     end
