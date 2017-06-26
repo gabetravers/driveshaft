7d6662c2372022ee115058630587a30140fc4d14
Gabe Travers <gtravers@hearst.com>
2017-06-26 16:42:01 -0400

    no message

--- lib/driveshaft/exports/spreadsheet.rb
+++ lib/driveshaft/exports/spreadsheet.rb
@@ -28,7 +28,7 @@ module Driveshaft
       end
 
       return {
-        body: JSON.dump(data).chop.gsub(":[",""),
+        body: JSON.dump(data).chop.gsub("\":[",""),
         content_type: 'application/json; charset=utf-8'
       }
     end
